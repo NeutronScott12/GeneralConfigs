7bc29cd99ffe588f90742565b2ecd0e20a013ebe
scott <scottberry91@gmail.com>
2021-11-01 23:43:16 +0000

    added windows version script

---
+++ powershell_scripts/GetWindowsVersionScript.ps1
@@ -0,0 +1,13 @@
+$name=(Get-WmiObject Win32_OperatingSystem).caption
+$bit=(Get-WmiObject Win32_OperatingSystem).OSArchitecture
+
+$vert = " Version:"
+$ver=(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ReleaseId
+
+$buildt = " Build:"
+$build= (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").BuildLabEx -match '^[0-9]+\.[0-9]+' |  % { $matches.Values }
+
+$installd = Get-ComputerInfo -Property WindowsInstallDateFromRegistry
+
+Write-host $installd
+Write-Host $name, $bit, $vert, $ver, `enter code here`$buildt, $build, $installd
